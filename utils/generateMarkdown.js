3251d0f96faac35471ec1f5187c45144e40d8de8
Cienna97 <cmortenson97@gmail.com>
2021-12-29 11:40:03 -0800

    add to function that returns a badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,9 +2,10 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if(license !== `None`){
-    return `![GitHub license](https://img.sheilds.io)`
+  if(license !== 'None'){
+    return `![GitHub license](https://img.shields.io/github/license/<Github-Username>/<Repository>)`;
   }
+  return'';
 }
 
 // TODO: Create a function that returns the license link
