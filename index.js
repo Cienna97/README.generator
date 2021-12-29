03dd1ce0436dceb73c8e5d03b1ddc3b50be6f688
Cienna97 <cmortenson97@gmail.com>
2021-12-29 14:19:09 -0800

    adding to the questions for user input

--- index.js
+++ index.js
@@ -5,7 +5,56 @@ const inquier = require('inquier');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your Github username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a description of your project'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license does your project need?',
+        choices: ['MIT', 'GPL 3.0', 'APACHE 2.0', 'BSD 3', 'None']
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What command to run tests?',
+        default: 'npm test'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What should user know about contributing to this repo?'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What command needs to be run to install the dependencies?',
+        default: 'npm i'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What should the user know about using this repo?'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
